06faf2c68293c6c2abc2a8179d775611e2ec6697
anmolsaini7055 <156368845+anmolsaini7055@users.noreply.github.com>
2025-07-23 04:22:41 +0530

    Add files via upload

---
+++ jarvis/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "despacito": "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+    "bohemian rhapsody": "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
+    # Add more songs here
+}
